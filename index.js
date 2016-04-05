d6aca38cc2ced1fc2ab86b582b1deb35ae0ae529
Matt Miller <thisismattmiller@gmail.com>
2016-04-05 16:56:13 -0400

    check for existing terms first...

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 function SerializeTerms () {
   /**
-   * A cluster script the spawns workers to build registry agents from sc:agents that have a VIAF id
+   * A cluster script the spawns workers to build registry terms from shadowcat terms
    *
    * @param  {function} cb - Nothing returned
    */
   this.shadowcatSerializeTerms = require(`${__dirname}/lib/shadowcat_serialize_terms`)
+
+  /**
+   * A cluster script the spawns workers to build registry terms from archives collections terms
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.archivesCollectionsSerializeTerms = require(`${__dirname}/lib/archives_serialize_collections_terms`)
 }
 
 module.exports = exports = new SerializeTerms()
